7f10594e6973258856450298375102d7ad3e4212
mihaim <mihai.moraru@foltortech.com>
2020-05-26 22:39:08 +0300

    default controller

---
+++ src/main/java/org/course/controllers/DefaultController.java
@@ -0,0 +1,18 @@
+package org.course.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/")
+    public String defaultEndPoint(){
+        return "Hello World Default";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
